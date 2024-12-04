cfcfcd8329806c745e8ee0b0b079f63a8ee195e8
pineappleboy187 <79086760+pineappleboy187@users.noreply.github.com>
2024-12-04 16:19:55 -0700

    Skeleton Code

---
+++ DLinkedList.py
@@ -0,0 +1,152 @@
+class DLinkedListNode:
+    # An instance of this class represents a node in Doubly-Linked List
+    def __init__(self, initData, initNext, initPrevious):
+        self.data = initData
+        self.next = initNext
+        self.previous = initPrevious
+
+        if initNext != None:
+            self.next.previous = self
+        if initPrevious != None:
+            self.previous.next = self
+
+    def getData(self):
+        return self.data
+
+    def setData(self, newData):
+        self.data = newData
+
+    def getNext(self):
+        return self.next
+
+    def getPrevious(self):
+        return self.previous
+
+    def setNext(self, newNext):
+        self.next = newNext
+
+    def setPrevious(self, newPrevious):
+        self.previous = newPrevious
+
+class DLinkedList:
+    # An instance of this class represents the Doubly-Linked List
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__size = 0
+
+
+    def insertion(self, pos, item):
+        '''
+        Adds a new node at the given position with item as its data.
+        :param pos: Index value of position that item will be added at.
+        :param item: Item to be added.
+        :return: None
+        '''
+
+        # Handle case where Position = 0 or at the head of the list.
+
+        if pos == 0:
+            temp = DLinkedListNode(item, self.__head, None)
+            if self.__head != None:
+                self.__head.setPrevious(temp)
+            else:
+                self.__tail = temp
+
+            self.__head = temp
+            self.__size += 1  # Increase the size of the list by one to account for inserted item.
+
+        # Handle case where Position = self.__size or rather tail position.
+        # Borrow from the append function.
+        elif pos == self.__size:
+            temp = DLinkedListNode(item, None, None)
+            if (self.__head == None):
+                self.__head = temp
+            else:
+                self.__tail.setNext(temp)
+                temp.setPrevious(self.__tail)
+
+            self.__tail = temp
+            self.__size += 1  # Increase the size of the list by one to account for inserted item.
+
+        # Handle case where Position is not head or tail.
+
+        elif 0 < pos < self.__size:
+
+            current = self.__head
+            count = 0
+            previous = None
+
+            while (count < pos):
+                previous = current
+                current = current.getNext()
+                count += 1
+
+            temp = DLinkedListNode(item, current, previous)
+            previous.setNext(temp)
+            current.setPrevious(temp)
+
+            self.__size += 1  # Increase the size of the list by one to account for inserted item.
+
+        else:
+            raise("Not a valid integer.") # Raises an error if the pos is not an integer.
+
+    def deletion(self, item):
+        # removes the first element in the list that is equal to the item
+
+        current = self.__head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.__head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+        if (current.getNext() != None):
+            current.getNext().setPrevious(previous)
+
+        else:
+            self.__tail = previous
+
+        self.__size -= 1
+
+    # Fix Later. Placeholder for now. Need to adapt for double linked list.
+    def sorting(self, data):
+        '''
+        Insertion sort algorithm. Sort the given Array with insertion sort method in Ascending order.
+        :return: None
+        '''
+        for index in range(1, len(data)):
+            temp = data[index]
+            position = index
+            while position > 0 and data[position - 1] > temp:
+                data[position] = data[position - 1]
+                position = position - 1
+
+            data[position] = temp
+
+
+    # Display Function
+    def __str__(self):
+        '''
+        Returns a string representation of the list
+        :return: string - a string representation of the list.
+        '''
+        # create the string representation of the linked list
+
+        # Similar implementation as seen in SLinkedList.py document and lecture slides.
+        current = self.__head
+        string = ''
+        while current != None:
+            string = string + str(current.getData())
+            if current.getNext() != None: # If the next item in the list, is not the end of the list.
+                string = string + " " # Ensures that there is a space between each item in the list.
+            current = current.getNext()
+        return string
--- Main.py
+++ Main.py
@@ -2,3 +2,69 @@ import time
 import os
 import random
 
+def groupStage():
+    pass
+# GROUP STAGE
+# Divide 32 teams into 8 groups of 4 teams each.
+
+# Each team plays a mathc against every other team in its groups.
+
+# Points are awarded. Win = 3 points, Draw = 1 point and Loss = 0 points.
+
+# After each match, update the linked list for each group.
+# Teams are ranked by points, and the top 2 teams from each group advance.
+
+def knockoutStage():
+    pass
+# KNOCKOUT STAGE
+# Conduct single-elimination rounds for the top 16 teams
+# (winners from the group stage)
+
+# Randomly pair teams for each knockout round in linked list, where each pair
+# is put in the list.
+
+# After each match, update the linked list by removing the losing team.
+
+# Continue rounds until one team emerges as the champion.
+
+def matchSimualtionStatistics():
+    pass
+# Implement randomized match results to simulate real game outcomes.
+# Track each team's wins, draws and losses. Store wins, losses, and draws directly.
+
+def display():
+    pass
+# Implement the display functions so users are able to:
+#   View current rankings, showing teams' standings and points after the group and knockout stages.
+#   Display detailed match statistics for each team, such as total wins, draws, and losses, after each group or knockout stages.
+#       Display a summary of match results for each game, a single table for each of the groups and knockout stages.
+
+def main():
+    pass
+
+# Load team data from teams.csv.
+
+# Organize teams into group-linked lists.
+
+# Run the group stage matches.
+
+# User Command: The users should be able to enter commands "S" to
+# see group rankings, or "C" to continue.
+
+# Run group and knockout stages sequentially by calling the appropriate functions.
+
+# User Command: The users should be able to enter commands "S" or "C" again.
+# This should print all the possible tables to give the user a complete picture
+# of all the games.
+
+# Display final tournament results, including the champion.
+# (If the games include a rematch to determine the winner due an
+# initial draw, format like: Croatia 1 - 1 Brazil (Croatia won 4-2
+# on penalties).
+
+# Output for group stage and knockout stage looks like the following.
+
+main()
+
+
+
